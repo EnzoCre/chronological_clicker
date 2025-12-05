6c953d4fab806a9711bdd7f4727cb8c1810caa00
FlorianWarin <74072575+FlorianWarin@users.noreply.github.com>
2025-12-05 18:29:38 +0100

    Delete era_clicker_v02-12 directory

--- era_clicker_v02-12/js/utils.js
+++
@@ -1,7 +0,0 @@
-export function formatNumber(num) {
-    return Math.floor(num).toLocaleString('fr-FR');
-}
-
-export function calculateCost(upgrade) {
-    return Math.ceil(upgrade.baseCost * Math.pow(upgrade.costMultiplier, upgrade.owned));
-}
